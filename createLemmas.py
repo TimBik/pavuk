5e230df0523062f839ab4d5dd08d48c6ae7e8fa7
Timbik <timurbikmullin@mail.ru>
2022-04-02 22:55:21 +0300

    add 5 tsak

--- createLemmas.py
+++ createLemmas.py
@@ -3,20 +3,23 @@ import pymorphy2
 morph = pymorphy2.MorphAnalyzer()
 
 name_file = "tokens/word_text_"
-for i in range(1, 107):
-    tokens = open(name_file + str(i) + ".txt", "r").readlines()
-    lems = {}
-    for token in tokens:
-        if (token[-1] == "\n"):
-            token = token[0:-1]
-        normal = morph.parse(token)[0].normal_form
-        if (normal not in lems):
-            lems[normal] = set()
-        lems[normal].add(token)
-    lem_file = open("lems/lem_" + str(i) + ".txt", "w")
-    for lem in lems:
-        lem_file.write(lem + ":  ")
-        for token in lem:
-            lem_file.write(token)
-        lem_file.write("\n")
-    print(str(i) + " done")
+for i in range(1, 150):
+    try:
+        tokens = open(name_file + str(i) + ".txt", "r").readlines()
+        lems = {}
+        for token in tokens:
+            if (token[-1] == "\n"):
+                token = token[0:-1]
+            normal = morph.parse(token)[0].normal_form
+            if (normal not in lems):
+                lems[normal] = set()
+            lems[normal].add(token)
+        lem_file = open("lems/lem_" + str(i) + ".txt", "w")
+        for lem in lems:
+            lem_file.write(lem + ":  ")
+            for token in lem:
+                lem_file.write(token)
+            lem_file.write("\n")
+        print(str(i) + " done")
+    except FileNotFoundError:
+        print('Файл не существует!')
--- getTokens.py
+++ getTokens.py
@@ -1,30 +1,34 @@
 from bs4 import BeautifulSoup
 import pymorphy2
+import os.path
 
 morph = pymorphy2.MorphAnalyzer()
 
 file_name_p = "vikachka/html_"
-i = 1
-for j in range(1, 149):
-    html = open(file_name_p + str(j) + ".txt", "r")
-    soup = BeautifulSoup(html, 'html.parser')
-    # текст из HTML-документа
-    text = soup.get_text()
-    text = text.replace('\n', ' ')
-    # text = text.replace('  ', ' ')
-    words = text.split(" ")
-    save = open("tokens/word_text_" + str(i) + ".txt", 'w')
-    unikum = set()
-    for word in words:
-        if (word != '' and not "же" in word):
-            res = morph.parse(word)[0]
-            if (res.is_known and res.tag.POS != "ADJF" and res.tag.POS != "PREP" and res.tag.POS != "CONJ"):
-                unikum.add(word)
-    if (len(unikum) == 0):
-        print("space")
-        continue
+for i in range(1, 149):
+    try:
+        html = open(file_name_p + str(i) + ".txt", "r")
 
-    for word in unikum:
-        save.write(word + "\n")
-    print(str(i) + " done")
-    i += 1
+        soup = BeautifulSoup(html, 'html.parser')
+        # текст из HTML-документа
+        text = soup.get_text()
+        text = text.replace('\n', ' ')
+        # text = text.replace('  ', ' ')
+        words = text.split(" ")
+        save = open("tokens/word_text_" + str(i) + ".txt", 'w')
+        unikum = set()
+        for word in words:
+            if (word != '' and not "же" in word):
+                res = morph.parse(word)[0]
+                if (res.is_known and res.tag.POS != "ADJF" and res.tag.POS != "PREP" and res.tag.POS != "CONJ"):
+                    unikum.add(word.lower())
+        if (len(unikum) == 0):
+            os.remove("tokens/word_text_" + str(i) + ".txt")
+            print("space")
+            continue
+
+        for word in unikum:
+            save.write(word + "\n")
+        print(str(i) + " done")
+    except FileNotFoundError:
+        print('Файл не существует!')
